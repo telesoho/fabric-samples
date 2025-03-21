bbce81aca2452000b5cc76b9fe829feb96f14492
telesoho <telesoho@gmail.com>
2025-03-21 09:12:03 +0900

    restapi: refactor

--- full-stack-asset-transfer-guide/applications/rest-api/src/server.ts
+++ full-stack-asset-transfer-guide/applications/rest-api/src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log('listening on port ' + PORT);
-})
+// app.listen(PORT, () => {
+//     console.log('listening on port ' + PORT);
+// })
+
